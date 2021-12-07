4a21ab9785fd863289c52c05938d7bb0958e93b1
asraavisena <asra.avisena@yahoo.com>
2021-12-07 11:32:38 +0700

    adding middleware error-handlers and revise responseHelpers

---
+++ src/middlewares/errorHandlers.js
@@ -0,0 +1,28 @@
+const httpStatus = require("http-status-codes");
+const resHelpers = require("@helpers/responseHelpers");
+
+async function errorHandler(error, req, res, next) {
+  switch (error.name) {
+    case "Bad Request":
+      res
+        .status(httpStatus.StatusCodes.BAD_REQUEST)
+        .json(resHelpers.failed(error.message, error.name || error));
+      break;
+    case "Not Found":
+      res
+        .status(httpStatus.StatusCodes.NOT_FOUND)
+        .json(resHelpers.failed(error.message, error.name));
+      break;
+    case "ValidationError":
+      res
+        .status(httpStatus.StatusCodes.BAD_REQUEST)
+        .json(resHelpers.failed(error._message, error.errors));
+      break;
+    default:
+      res
+        .status(httpStatus.StatusCodes.INTERNAL_SERVER_ERROR)
+        .json(resHelpers.failed(error.message, error.name || error));
+      break;
+  }
+}
+module.exports = { errorHandler };
