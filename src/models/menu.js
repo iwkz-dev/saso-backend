fc94366738df6b83fa74c26f60526f204fd33c94
asraavisena <asra.avisena@yahoo.com>
2022-01-03 09:24:14 +0700

    add even object id

--- src/models/menu.js
+++ src/models/menu.js
@@ -17,6 +17,10 @@ const menuSchema = new mongoose.Schema({
     min: [0, "Cannot be lower than 0"],
     required: [true, "Quantity is required"],
   },
+  event: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Events",
+  },
   price: {
     type: Number,
     min: [0, "Cannot be lower than 0"],
