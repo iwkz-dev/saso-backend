3432c1586d6827bc7cc74076b3efccdb3d762778
Muhammad Juan Akbar <mail@mjuan.info>
2022-01-18 22:03:51 +0100

    change swagger servers url

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -9,8 +9,8 @@ const options = {
       description: "Saso API",
     },
     servers: [
-      { url: "http://localhost:3000/api/v1" },
-      { url: "https://saso.iwkz.de/api/api/v1" },
+      { url: `${process.env.PROD_API_URL}/api/v1` },
+      { url: `http://localhost:${process.env.LOCAL_PORT_API}/api/v1` },
     ],
   },
   apis: ["src/docs/*.yml", "./src/routes/*.js", "./src/routes/*/*.js"], // files containing annotations as above
