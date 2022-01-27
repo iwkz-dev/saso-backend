268f7f5f0eb2e5f81fa85983927d223650d32a6b
asraavisena <asra.avisena@yahoo.com>
2022-01-27 14:01:00 +0700

    fix error no access token, in localhost it works

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,24 +5,19 @@ const User = require("@models/user");
 
 async function authAdmin(req, res, next) {
   // ! DI HEADERS TAMBAH -> access_token
-  const { access_token: accessToken } = req.headers;
+  // const { access_token: accessToken } = req.headers;
+  const { authorization } = req.headers;
+  const accessToken = authorization && authorization.split(" ")[1];
 
   try {
     if (!accessToken) {
-      throw { name: "No Access Token", message: req.headers };
+      throw { name: "Invalid Auth", message: "Invalid Access Token" };
     } else {
       const verifiedAccessToken = jwtVerify(accessToken);
-      console.log(
-        "🚀 ~ file: auth.js ~ line 15 ~ authAdmin ~ verifiedAccessToken",
-        verifiedAccessToken
-      );
 
       const findUser = await User.findById(verifiedAccessToken.id);
       if (!findUser || findUser.role === 3) {
-        throw {
-          name: "User and role not fit",
-          message: "Invalid Access Token",
-        };
+        throw { name: "Invalid Auth", message: "Invalid Access Token" };
       } else {
         req.user = verifiedAccessToken;
         next();
@@ -35,20 +30,18 @@ async function authAdmin(req, res, next) {
 }
 
 async function authSuperAdmin(req, res, next) {
-  const { access_token: accessToken } = req.headers;
+  const { authorization } = req.headers;
+  const accessToken = authorization && authorization.split(" ")[1];
 
   try {
     if (!accessToken) {
-      throw { name: "In auth sa no token", message: "Invalid Access Token" };
+      throw { name: "Invalid Auth", message: "Invalid Access Token" };
     } else {
       const verifiedAccessToken = jwtVerify(accessToken);
 
       const findUser = await User.findById(verifiedAccessToken.id);
       if (!findUser || findUser.role !== 1) {
-        throw {
-          name: "In auth sa no fit role and user ",
-          message: "Invalid Access Token",
-        };
+        throw { name: "Invalid Auth", message: "Invalid Access Token" };
       } else {
         next();
       }
@@ -61,7 +54,8 @@ async function authSuperAdmin(req, res, next) {
 
 async function authCustomer(req, res, next) {
   // ! DI HEADERS TAMBAH -> access_token
-  const { access_token: accessToken } = req.headers;
+  const { authorization } = req.headers;
+  const accessToken = authorization && authorization.split(" ")[1];
 
   try {
     if (!accessToken) {
