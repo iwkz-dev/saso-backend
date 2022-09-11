6e27e44f7e6d7bc70bc47dbf2b2310314cd13395
asraavisena <asra.avisena@yahoo.com>
2022-09-11 16:21:30 +0700

    fixing generatepdf

--- src/helpers/pdfGenerator.js
+++ src/helpers/pdfGenerator.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 module.exports = {
   pdfGenerator: async function (html = "") {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
     await page.setContent(html);
