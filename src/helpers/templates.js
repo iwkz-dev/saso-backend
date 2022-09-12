44b6a3dd23cda6e7596caeeaa38d2619ee67cd92
asraavisena <asra.avisena@yahoo.com>
2022-09-12 16:24:42 +0700

    templates edit

--- src/helpers/templates.js
+++ src/helpers/templates.js
@@ -62,7 +62,7 @@ module.exports = {
     return {
       from: "noreply@gmail.com",
       to: `${email}`,
-      subject: "Verification Email",
+      subject: "Change Password",
       html: `
   
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
