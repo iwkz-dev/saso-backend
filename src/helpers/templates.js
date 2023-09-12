34e9f5631346e54aebac3869730298e3821bcd1f
ilhamuh97 <ilhamuh97@gmail.com>
2023-09-12 20:45:57 +0200

    add more info in invoice

--- src/helpers/templates.js
+++ src/helpers/templates.js
@@ -599,6 +599,14 @@ module.exports = {
                                                      }</span>
                                                     </td>
                                                 </tr>
+                                                <tr>
+                                                <td colspan="2"
+                                                  style="width: 100%; text-align: center; font-size: 12px; color: #f1807e; padding-top: 12px 0">
+                                                    <span style="font-weight: 600;">
+                                                      Unpaid orders will be canceled automatically within 2x24 hours. Please send the proof to the contact person.
+                                                    </span>
+                                                </td>
+                                            </tr>
                                               </tbody>
                                           </table>
                                       </td>
